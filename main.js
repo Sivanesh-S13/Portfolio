db3243ec44d893ba680fac774edb4d99ef6880d3
SIVANESH S <84979194+Sivanesh-S13@users.noreply.github.com>
2024-02-12 01:46:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var Typed = new Typed(".text",{
-    Strings:["Software Developer","Fullstack Developer","Problem Solver","Influencer","Tech Enthusiast","Youtuber"],
+var typed = new Typed(".text",{
+    strings:["Software Developer","Fullstack Developer","Problem Solver","Influencer","Tech Enthusiast","Youtuber"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
